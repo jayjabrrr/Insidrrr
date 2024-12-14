84e70cee846172164e720ec28f2269e2574c7c0e
jay <jay@insidr.app>
2024-12-13 16:44:42 -0800

    Initial Commit

---
+++ Insidr/InsidrApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsidrApp.swift
+//  Insidr
+//
+//  Created by Jay Jaber on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct InsidrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InsidrTests/InsidrTests.swift
@@ -0,0 +1,16 @@
+//
+//  InsidrTests.swift
+//  InsidrTests
+//
+//  Created by Jay Jaber on 12/13/24.
+//
+
+import Testing
+
+struct InsidrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
